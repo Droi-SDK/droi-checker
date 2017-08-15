7ca6b095f07346a220126a47bb2a1a26646fc8ca
marekchen <marek@163.com>
2017-08-15 22:31:00 +0800

    [tag] update version 0.1.1

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 import "github.com/Droi-SDK/droi-checker/logger"
 
-const Version = "0.1.0"
+const Version = "0.1.1"
 
 func PrintCurrentVersion() {
 	logger.Info("当前命令行工具版本：", Version)
